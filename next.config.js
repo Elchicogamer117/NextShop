a7c74a604d3891df785d91a947e23b4fa2110d5b
Cristian <cris18002011@hotmail.com>
2022-06-28 14:52:34 -0500

    Next configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  env: {
+    customKey: 'customValue',
+  },
+    //basePath: '/dist', final option
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source : '/hola',
+        destination: 'https://github.com/Elchicogamer117', 
+        permanent: true,                          
+      }
+    ]
+  }
 }
-
-module.exports = nextConfig
