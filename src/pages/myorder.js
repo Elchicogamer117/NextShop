317f1c5954a97771ab769f12bfda34a701e78ff3
Cristian <cris18002011@hotmail.com>
2022-07-01 16:52:43 -0500

    Adding Google Analytics Settings

--- src/pages/myorder.js
+++ src/pages/myorder.js
@@ -1,26 +1,30 @@
 import React from 'react'
+import Head from 'next/head'
 import OrderItem from 'components/OrderItem'
 import styles from 'styles/pages/MyOrder.module.scss'
 
 const MyOrder = () => {
   return (
-    <div className={styles.myOrder}>
-
-      <div className={styles.myOrderContainer}>
-        <h1 className={styles.title}> My order</h1>
-
-        <div className={styles.myOrderContent}>
-          <div className={styles.order}>
-            <p>
-              <span> 04.25.2021</span>
-              <span> 6 articles</span>
-            </p>
-            <p> $560 </p>
+    <>
+      <Head>
+        <title> My order </title>
+      </Head>
+      <div className={styles.myOrder}>
+        <div className={styles.myOrderContainer}>
+          <h1 className={styles.title}> My order</h1>
+          <div className={styles.myOrderContent}>
+            <div className={styles.order}>
+              <p>
+                <span> 04.25.2021</span>
+                <span> 6 articles</span>
+              </p>
+              <p> $560 </p>
+            </div>
           </div>
+          <OrderItem />
         </div>
-        <OrderItem />
       </div>
-    </div>
+    </>
   )
 }
 
