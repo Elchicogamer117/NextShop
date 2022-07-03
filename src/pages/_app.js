960c01c4a2c2b401a89547f1c977b3be582c6be0
Cristian <cris18002011@hotmail.com>
2022-07-02 23:45:46 -0500

    Add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,9 @@
 import Script from 'next/script';
+import Head from 'next/head';
 import AppContext from 'context/AppContext';
 import useInitialState from 'hooks/useInitialState';
 import Header from 'components/Header';
-import '../styles/globals.css';
+import '../styles/globals.modules.scss';
 
 function MyApp({ Component, pageProps }) {
   const initialState = useInitialState();
@@ -17,6 +18,10 @@ function MyApp({ Component, pageProps }) {
           gtag('config', 'G-Y8T3JGFH1F');
         `}
       </Script>
+      <Head>
+        <title> Next Shop </title>
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </AppContext.Provider>
