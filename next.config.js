d220f7999f1c9af8cff40cb911f7d8574003fc53
Cristian <cris18002011@hotmail.com>
2022-07-04 19:37:40 -0500

    Deploy as pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    mode: 'production',
+    disable: false,
+  },
   reactStrictMode: true,
   images: {
-    domains: ['placeimg.com', 'api.lorem.space', 'vuzoon.com', 'www.complementosdelcafe.com', 'placeing.com', 'source.unsplash.com', 'picsum.photos', 'images.pexels.com'],
+    domains: ['placeimg.com', 'api.lorem.space', 'vuzoon.com', 'www.complementosdelcafe.com', 'placeing.com', 'source.unsplash.com', 'picsum.photos', 'images.pexels.com', 'www.adidas.co'],
   },
-};
+});
